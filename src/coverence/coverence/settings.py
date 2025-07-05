202c78d7e045f8653a55d71c778f9d9a6ae3d4cd
Gautham <gautham@Gauthams-MacBook-Air.local>
2025-07-06 00:04:26 +0530

    Updated API URLs, WebSocket URLs, and CORS settings for deployment

--- src/coverence/coverence/settings.py
+++ src/coverence/coverence/settings.py
@@ -19,7 +19,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,coverence-backend.onrender.com").split(",")
+
 
 
 # Application definition
